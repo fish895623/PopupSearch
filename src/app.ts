f544cdb9a4e7a5e14fb28d663e445a2d9e1d6511
fish895623 <dan990429@gmail.com>
2021-03-28 16:45:27 +0900

    Global ShortCut Fixes #1

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,29 @@
-import { app, BrowserWindow } from 'electron'
+import {app, BrowserWindow, globalShortcut} from 'electron'
 
 let win = null
+
 app.on('ready', () => {
     win = new BrowserWindow()
     // win.setAlwaysOnTop(true, undefined, undefined)
     win.loadURL('http://localhost:5656/')
-    win.on('minimize', (error) => {
-        console.log(error)
+})
+
+// 플랫폼이 맥os 이면 앞의 것을 선택, 아닐경우 뒤에것을 선택하여 값을 입력
+const refreshCommand = process.platform === 'darwin' ? 'Cmd+R' : 'F5'
+
+app.on('browser-window-focus', () => {
+    globalShortcut.register(refreshCommand, () => {
+        app.quit()
+    })
+})
+
+app.whenReady().then(() => {
+    globalShortcut.register('Alt+I', () => {
+        if (win.isFocused() == false) {
+            win.focus()
+        } else {
+            win.minimize()
+        }
     })
 })
 
